cadd713640633348088979ec8be2ccbc39e315aa
Arthur Baroi <arthurcbaroi@hotmail.com>
2026-08-29 18:04:02 -0500

    Removed Default Library and Script Files.

--- IniTypeProvider/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace IniTypeProvider
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
--- IniTypeProvider/Script.fsx
+++
@@ -1,8 +0,0 @@
-﻿// Learn more about F# at https://fsharp.org
-// See the 'F# Tutorial' project for more help.
-
-#load "Library1.fs"
-open IniTypeProvider
-
-// Define your library scripting code here
-
