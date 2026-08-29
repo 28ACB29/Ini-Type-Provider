63a8e50a6aa5119fdc0044a18bf78384dde01095
Arthur Baroi <arthurcbaroi@hotmail.com>
2026-08-29 11:37:12 -0500

    Removed the redundant RuntimeLoad in RuntimeParse, and renamed Runtime to RuntimeLoad.

--- IniTypeProvider/RuntimeParse.fs
+++ IniTypeProvider/RuntimeParse.fs
@@ -2,29 +2,6 @@
 
 open System
 
-open IniDesign
-open Parsing
-
-type IniRuntimeSection = IniRuntimeSection of Map<string,string>
-
-type IniRuntimeDocument = IniRuntimeDocument of Map<string, IniRuntimeSection> with
-    member this.TryGetSection (name:string):IniRuntimeSection option =
-        let (IniRuntimeDocument sections) = this
-        Map.tryFind name sections
-
-    member this.TryGet(section, key) =
-        match this.TryGetSection section with
-        | None -> None
-        | Some (IniRuntimeSection m) -> Map.tryFind key m
-
-module RuntimeLoad =
-    let loadRuntime (path:string):IniRuntimeDocument =
-        let ini:Map<string, Map<string, string>> = parseIniFile path
-        let sections:Map<string, IniRuntimeSection> =
-            ini
-            |> Map.map (fun _ (keys:Map<string, string>) -> IniRuntimeSection keys)
-        IniRuntimeDocument sections
-
 // ------------------------------------------------------------
 // NEW: Safe parsing helpers callable from inside quotations
 // ------------------------------------------------------------
