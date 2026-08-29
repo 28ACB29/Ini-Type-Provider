4bb4fa51706486b8aef389ae55ce3fe743d51919
Arthur Baroi <arthurcbaroi@hotmail.com>
2026-08-29 11:59:03 -0500

    Added missing types to Getters Module.

--- IniTypeProvider/Getters.fs
+++ IniTypeProvider/Getters.fs
@@ -1,6 +1,7 @@
 ﻿namespace IniTypedProvider
 
 open Microsoft.FSharp.Quotations
+open System
 
 open Defaults
 open DesignMerge
@@ -10,56 +11,57 @@ open RuntimeParse
 module Getters =
 
     let getterExpr (info:KeyDesignInfo) (docExpr:Expr):Expr =
-        let sec, key = info.Section, info.Key
-        let t = info.InferredType
-        let propType = propertyType info
-        let isOption =
+        let section:string = info.Section
+        let key:string = info.Key
+        let t:Type = info.InferredType
+        let propType:Type = propertyType info
+        let isOption:bool =
             propType.IsGenericType &&
             propType.GetGenericTypeDefinition() = typedefof<option<_>>
 
-        let defaultObjOpt =
+        let defaultObjOpt:objnull option =
             info.TypedDefault |> Option.map boxDefault
 
-        let blockDefaultObjOpt =
+        let blockDefaultObjOpt:objnull option =
             info.BlockDefault
-            |> Option.bind (fun raw ->
+            |> Option.bind (fun (raw:string) ->
                 if t = typeof<string> then Some (box raw)
                 elif t = typeof<bool> then parseBool raw |> Option.map box
                 else parseNumeric t raw)
 
         if not isOption then
             <@@
-                let doc = %%docExpr : IniRuntimeDocument
-                match doc.TryGet(sec, key) with
-                | Some raw ->
+                let document:IniRuntimeDocument = %%docExpr : IniRuntimeDocument
+                match document.TryGet(section, key) with
+                | Some (raw:string) ->
                     if t = typeof<string> then box raw
                     elif t = typeof<bool> then
                         match parseBool raw with
-                        | Some v -> box v
+                        | Some (v:bool) -> box v
                         | None ->
                             match defaultObjOpt, blockDefaultObjOpt with
-                            | Some d, _ -> d
-                            | None, Some d -> d
-                            | None, None -> failwithf "Invalid bool for %s.%s" sec key
+                            | Some (d:objnull), _ -> d
+                            | None, Some (d:objnull) -> d
+                            | None, None -> failwithf "Invalid bool for %s.%s" section key
                     else
                         match parseNumeric t raw with
-                        | Some v -> v
+                        | Some (v:objnull) -> v
                         | None ->
                             match defaultObjOpt, blockDefaultObjOpt with
-                            | Some d, _ -> d
-                            | None, Some d -> d
-                            | None, None -> failwithf "Invalid numeric for %s.%s" sec key
+                            | Some (d:objnull), _ -> d
+                            | None, Some (d:objnull) -> d
+                            | None, None -> failwithf "Invalid numeric for %s.%s" section key
                 | None ->
                     match defaultObjOpt, blockDefaultObjOpt with
-                    | Some d, _ -> d
-                    | None, Some d -> d
-                    | None, None -> failwithf "Missing required key %s.%s" sec key
+                    | Some (d:objnull), _ -> d
+                    | None, Some (d:objnull) -> d
+                    | None, None -> failwithf "Missing required key %s.%s" section key
             @@>
         else
             <@@
-                let doc = %%docExpr : IniRuntimeDocument
-                match doc.TryGet(sec, key) with
-                | Some raw ->
+                let document:IniRuntimeDocument = %%docExpr : IniRuntimeDocument
+                match document.TryGet(section, key) with
+                | Some (raw:string) ->
                     if t = typeof<string> then Some (box raw)
                     elif t = typeof<bool> then parseBool raw |> Option.map box
                     else parseNumeric t raw
